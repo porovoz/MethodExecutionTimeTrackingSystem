d6ae9422d4fc989070d9bdb8bd2dcf260a4c6990
porovoz <falkon1@list.ru>
2024-03-30 17:59:03 +0300

    initial commit

---
+++ src/main/java/com/bestapp/MethodExecutionTimeTrackingSystem/MethodExecutionTimeTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bestapp.MethodExecutionTimeTrackingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MethodExecutionTimeTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MethodExecutionTimeTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bestapp/MethodExecutionTimeTrackingSystem/MethodExecutionTimeTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestapp.MethodExecutionTimeTrackingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MethodExecutionTimeTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
