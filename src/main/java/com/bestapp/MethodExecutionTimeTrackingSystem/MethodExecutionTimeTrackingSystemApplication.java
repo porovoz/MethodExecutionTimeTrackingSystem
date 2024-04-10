f98f195905c9b0d32bd8c65ca36f790ea79dab0d
porovoz <falkon1@list.ru>
2024-04-10 19:13:16 +0300

    @EnableAsync annotation added.

--- src/main/java/com/bestapp/MethodExecutionTimeTrackingSystem/MethodExecutionTimeTrackingSystemApplication.java
+++ src/main/java/com/bestapp/MethodExecutionTimeTrackingSystem/MethodExecutionTimeTrackingSystemApplication.java
@@ -2,8 +2,10 @@ package com.bestapp.MethodExecutionTimeTrackingSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MethodExecutionTimeTrackingSystemApplication {
 
 	public static void main(String[] args) {
